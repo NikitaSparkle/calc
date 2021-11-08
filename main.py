9abac11323a9ab88d2310fb8b506d6c56ae64da5
Alexey <irresjeny@gmail.com>
2021-11-08 10:48:33 +0200

    0.2

--- main.py
+++ main.py
@@ -2,4 +2,11 @@ print('First num')
 num1 = input()
 print('Second num')
 num2 = input()
-print(int(num1) + int(num2))
+print('Action')
+action = input()
+if action == '/':
+    print(int(num1) / int(num2))
+elif action == '+':
+    print(int(num1) + int(num2))
+else:
+    print('error')
