dab3663bcf3f6d094125a142a6256cff770a07e4
Irresjeny <irresjeny@gmail.com>
2021-11-15 09:51:56 +0200

    Господи спаси

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 print('First num')
 num1 = input()
+
 print('Second num')
 num2 = input()
+
 print('Action')
 action = input()
+
 if action == '/':
     if num2 == '0':
         print('error')
     else:
         print(int(num1) / int(num2))
+
 elif action == '+':
     print(int(num1) + int(num2))
+
 if action == '*':
     print(int(num1) * int(num2))
 else:
     print('error')
+
+if action == '^':
+    print(int(num1)**int(num2))
+else:
+    print('error')
+
